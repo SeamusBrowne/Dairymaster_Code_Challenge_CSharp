fde080d319058aab20dd400f3d93209065cc9057
Seamus Browne <31517569+SeamusBrowne@users.noreply.github.com>
2025-05-09 08:55:43 +0100

    Update UnitTest1.cs

--- IntegrationTest/UnitTest1.cs
+++ IntegrationTest/UnitTest1.cs
@@ -73,6 +73,37 @@ public class UnitTest1 : WebApplicationFactory<Program>
 
     }
 
+        // Tests book publishing of a created book
+    [Fact]
+    public async Task Test5_CreatePublishBook()
+    {
+        var createPublishBook = new Book
+        {
+            Title = "Book For Publish",
+            PubStatus = "Planned",
+            PubDate = ""
+        };
+
+        var client = CreateClient();
+        var createResponse = await client.PostAsJsonAsync("/createbook", createPublishBook);
+        createResponse.EnsureSuccessStatusCode();
+
+        var createdBook = await createResponse.Content.ReadFromJsonAsync<Book>();
+        createdBook.Should().NotBeNull();
+        createdBook!.Title.Should().Be("Book For Publish");
+        createdBook.PubStatus.Should().Be("Planned");
+
+
+        var response = await client.PatchAsJsonAsync<Book>($"/publishbook?bid={createdBook.Id}", createdBook);
+        response.EnsureSuccessStatusCode();
+        var publishedBook = await response.Content.ReadFromJsonAsync<Book>();
+        publishedBook!.Id.Should().Be(createdBook.Id);
+        publishedBook.PubStatus.Should().Be("Published"); 
+        publishedBook.PubDate.Should().Be(DateTime.Now.ToString("yyyy-MM-dd"));
+        publishedBook.PubDate.Should().NotBeNullOrEmpty();
+
+    }
+
     public class Book
     {
         public int? Id { get; set; }
